fd294cedf065da6f933e28a48aada66c8df13820
Mc5766 <priyansharma1981@gmail.com>
2022-09-25 23:59:32 +0530

    products API and data binding and Cart functionality

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,11 @@
 const BASE_URL = "http://localhost:5000";
+const getQueryString = new URLSearchParams(window.location.search);
+const DOM_PARSER = new DOMParser();
+const CART_ITEMS = [];
+window.addEventListener("load", async () => {
+  bindUserProfile();
+  bindProducts();
+});
 
 const formValidation = (formType) => {
   let elements = document.querySelectorAll("input");
@@ -6,13 +13,15 @@ const formValidation = (formType) => {
     if (i === elements.length) {
       if (formType === "signup") {
         createAccountHandler();
+      } else if(formType === "payment"){
+        paymentHandler();
       } else {
         loginHandler();
       }
       break;
     }
     if (elements[i].value == "") {
-      alert(`You Missed ${elements[i].name}`);
+      alert(`You missed ${elements[i].name}`);
       elements[i].focus();
       break;
     }
@@ -65,9 +74,9 @@ const loginHandler = async () => {
     });
     let getData = await getJson.json();
     if (getData.code == 0) {
-      localStorage.setItem('islogin', JSON.stringify(getData));
-      window.location.href = '/';
-    }else{
+      localStorage.setItem("islogin", JSON.stringify(getData));
+      window.location.href = "/";
+    } else {
       alert(getData.message);
     }
   } catch (error) {
@@ -75,24 +84,90 @@ const loginHandler = async () => {
   }
 };
 
-const logoutHandler = () =>{
-  localStorage.clear();
-  window.location.href = "/"
+const paymentHandler = () => {
+  alert("payment Hit")
 }
 
-window.addEventListener('load', async () => {
-  const getQueryString = new URLSearchParams(window.location.search);
-  const getUserId = getQueryString.get("user")
-  if(getUserId){
+const logoutHandler = () => {
+  localStorage.clear();
+  window.location.href = "/";
+};
+
+const bindUserProfile = async () => {
+  const getUserId = getQueryString.get("user");
+  if (getUserId) {
     const getJSON = await fetch(`${BASE_URL}/getprofile?userID=${getUserId}`);
     const getData = await getJSON.json();
-    if(getData.code == 1 || getData.length <= 0){
-      document.getElementById('userName').innerHTML = `${getData.message}`;
-    }else{
-      document.getElementById('userName').innerHTML = `${getData[0].firstName} ${getData[0].lastName}`
-      document.getElementById('userEmail').innerHTML = `Email- ${getData[0].email}`
-      document.getElementById('userPhone').innerHTML = `Phone- ${getData[0].mobileNumber}`
-
+    if (getData.code == 1 || getData.length <= 0) {
+      document.getElementById("userName").innerHTML = `${getData.message}`;
+    } else {
+      document.getElementById(
+        "userName"
+      ).innerHTML = `${getData[0].firstName} ${getData[0].lastName}`;
+      document.getElementById(
+        "userEmail"
+      ).innerHTML = `Email- ${getData[0].email}`;
+      document.getElementById(
+        "userPhone"
+      ).innerHTML = `Phone- ${getData[0].mobileNumber}`;
     }
   }
-})
+};
+
+const cartHandler = (event, id, price, orgPrice, title, image) => {
+  event.setAttribute("disabled", "true");
+  event.innerHTML = "Item Added";
+  event.style.backgroundColor = "#bbb";
+  ++document.getElementsByClassName("cartNumber")[0].innerHTML;
+  let itemObject = {
+    id: id,
+    price: price,
+    orgPrice: orgPrice,
+    title: title,
+    image: image,
+  };
+  CART_ITEMS.push(itemObject);
+  sessionStorage.setItem("item", JSON.stringify(CART_ITEMS));
+};
+
+const validateCart = (element) => {
+  if (element.children[1].innerHTML == 0) {
+    alert("Please add at-least one item to cart");
+    return;
+  }
+  element.setAttribute("href", "/cart");
+};
+
+const bindProducts = async () => {
+  const getCategoryId = getQueryString.get("category");
+  if (getCategoryId) {
+    const getJson = await fetch(
+      `${BASE_URL}/getproducts?categoryID=${getCategoryId}`
+    );
+    const getData = await getJson.json();
+    if (getData && getData.length > 0) {
+      const getHTML = getData.map((value) => {
+        return `<div class="w-80 bg-white shadow-lg rounded-2xl overflow-hidden border border-solid border-[#bbb] md:m-8 mb-8">
+          <img src=${value.image} class="w-full" />
+          <div class="flex flex-col">
+            <div class="mb-4 px-2">
+              <p class="text-lg font-semibold flex justify-between"> <span class="text-zinc-600 line-through"> &#8377;${value.originalPrice}/- </span> <span class="text-[#FF5733]">Rating: ${value.rating}</span> </p>
+              <p class="text-2xl font-extrabold mb-1">&#8377; ${value.price}/-</p>
+              <p class="text-xl font-medium -mt-1">${value.title}</p>
+            </div>
+            <div class="text-right mx-5 mb-3">
+              <button class="bg-black text-white rounded-full px-3 py-2 text-sm" onclick="cartHandler(this,'${value.productID}', '${value.price}', '${value.originalPrice}', '${value.title}', '${value.image}')">Add To Cart</button>
+            </div>
+          </div>
+        </div>`;
+      });
+      for (let i = 0; i < getHTML.length; i++) {
+        const convertHTML = DOM_PARSER.parseFromString(
+          getHTML[i],
+          "text/html"
+        ).body;
+        document.getElementById("productsList").append(convertHTML);
+      }
+    }
+  }
+};
---
+++ public/js/payment.js
@@ -0,0 +1,38 @@
+window.addEventListener("load", () => {
+  let getCartItems = sessionStorage.getItem("item");
+  getCartItems = JSON.parse(getCartItems);
+  const pricesList = [];
+  let getHTMLcartItem = getCartItems.map((value) => {
+    return `<div class="md:w-52 w-64 h-full bg-white shadow-lg rounded-2xl overflow-hidden border border-solid border-[#bbb] ml-4 lg:mb-4 md:mb-4 mb-4">
+          <img src=${value.image} class="w-full" />
+          <div class="flex justify-between items-center">
+              <div class="mb-4 px-2">
+                  <p class="text-sm font-semibold flex justify-between mt-1"> <span class="text-zinc-500 line-through"> &#8377; ${value.orgPrice}/- </span> </p>
+                  <p class="text-lg font-extrabold">&#8377; ${value.price}/-</p>
+                  <p class="text-md font-medium">${value.title}</p>
+              </div>
+          </div>
+      </div>`;
+  });
+  let getHTMLpriceList = getCartItems.map((value) => {
+    pricesList.push(value.price)
+    return `<div><p class="flex justify-between mb-1 pb-1 border-b border-slate-300">
+        <span class="text-lg">${value.title}</span>
+        <span class="text-lg font-bold">&#8377; ${value.price}/-</span>
+      </p><div>`;
+  });
+  let grandTotal = pricesList.reduce((acc, curr) => {
+    return parseInt(acc) + parseInt(curr)
+  })
+  document.getElementById("grandTotal").innerHTML = `&#8377; ${grandTotal}/-`
+  bindHTML(getHTMLcartItem, "cartItem");
+  bindHTML(getHTMLpriceList, "priceList");
+});
+
+const bindHTML = (htmlString, parentId) => {
+  for (let i = 0; i < htmlString.length; i++) {
+    const DOM_PARSER = new DOMParser();
+    const convertHTML = DOM_PARSER.parseFromString(htmlString[i], "text/html").body;
+    document.getElementById(parentId).append(convertHTML.querySelector("div"));
+  }
+}
